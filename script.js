8e984eaf4c8ab9ff8644de7d8c777738c35d6f55
Dina <dinagiacobbi@gmail.com>
2022-06-26 10:11:49 -0800

    Simple Todo List

---
+++ script.js
@@ -0,0 +1,66 @@
+const form = document.getElementById("form");
+const input = document.getElementById("input");
+const todosUL = document.getElementById("todos");
+
+//use the brower API to access local storage-- JSON needed to parse
+const todos = JSON.parse(localStorage.getItem("todos"));
+
+if (todos) {
+  todos.forEach((todo) => addTodo(todo));
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  addTodo();
+});
+
+function addTodo(todo) {
+  let todoText = input.value;
+
+  if (todo) {
+    todoText = todo.text;
+  }
+
+  if (todoText) {
+    const todoElement = document.createElement("li");
+    if (todo && todo.completed) {
+      todoElement.classList.add("completed");
+    }
+
+    todoElement.innerHTML = todoText;
+
+    todoElement.addEventListener("click", () => {
+      todoElement.classList.toggle("completed");
+      updateLocalStorage();
+    });
+
+    todoElement.addEventListener("contextmenu", (e) => {
+      e.preventDefault();
+
+      todoElement.remove();
+      updateLocalStorage();
+    });
+
+    todosUL.appendChild(todoElement);
+
+    input.value = "";
+
+    updateLocalStorage();
+  }
+}
+
+function updateLocalStorage() {
+  todosElement = document.querySelectorAll("li");
+
+  const todos = [];
+
+  todosElement.forEach((todoElement) => {
+    todos.push({
+      text: todoElement.innerText,
+      completed: todoElement.classList.contains("completed"),
+    });
+  });
+
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
